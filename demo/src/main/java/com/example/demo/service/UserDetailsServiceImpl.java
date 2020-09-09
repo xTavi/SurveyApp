d2127b549dea1801455409bd33147f3d1c559255
Tavi <taviatwork@gmail.com>
2020-09-09 20:18:21 +0300

    I think this changes are not ok

---
+++ demo/src/main/java/com/example/demo/service/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.example.demo.service;
+
+import com.example.demo.model.MyUserDetails;
+import com.example.demo.model.User;
+import com.example.demo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+        User user = userRepository.getUserByUsername(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("Could not find user");
+        }
+
+        return new MyUserDetails(user);
+    }
+
+}
