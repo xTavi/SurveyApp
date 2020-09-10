2c2b480e7aae61e6c16b8167842deb8690a9e265
Tavi <taviatwork@gmail.com>
2020-09-10 21:25:31 +0300

    Added web security WORKING

--- demo/src/main/java/com/example/demo/SecurityConfiguration.java
+++ demo/src/main/java/com/example/demo/SecurityConfiguration.java
@@ -1,11 +1,9 @@
 package com.example.demo;
 
-import com.example.demo.service.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -14,26 +12,29 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private MyUserDetailsService myUserDetailsService;
-
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/h2-console/**").hasRole("ADMIN")//allow h2 console access to admins only
-                .anyRequest().authenticated()//all other urls can be access by any authenticated role
-                .and().formLogin()//enable form login instead of basic login
-                .and().csrf().ignoringAntMatchers("/h2-console/**")//don't apply CSRF protection to /h2-console
-                .and().headers().frameOptions().sameOrigin();//allow use of frame to same origin urls
+                .antMatchers("/h2-console/**").hasRole("ADMIN")
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/api/v1/**").hasRole("ADMIN")
+                .antMatchers("/").permitAll()
+//                .anyRequest().authenticated()//all other urls can be access by any authenticated role
+                .and().formLogin()
+                .and().csrf().ignoringAntMatchers("/h2-console/**") // don't apply CSRF protection to /h2-console;
+                .and().csrf().ignoringAntMatchers("/login")
+                .and().headers().frameOptions().sameOrigin(); // allow use of frame to same origin urls
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
         auth.inMemoryAuthentication()
                 .withUser("blah")
                 .password("blah")
                 .roles("ADMIN")
-        .and();
+                .and()
+                .withUser("yo")
+                .password("yo")
+                .roles("USER");
     }
 
     @Bean
